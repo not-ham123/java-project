3ddf139e6a9201789138592990009d0e3dc6bc69
not-ham123 <98446206+not-ham123@users.noreply.github.com>
2022-06-03 19:28:05 +0700

    Create Article.java

---
+++ Article.java
@@ -0,0 +1,77 @@
+package com.fun.scrapescrapefx;
+
+public class Article 
+    
+{
+
+    private String title;
+
+    private String linkToHeaderPicture;
+    private String text;
+
+    private String source;
+    private String timeSincePosted;
+    private String linkToArticle;
+
+
+
+    public String getText() 
+    {
+        return text;
+    }
+
+    public void setText(String text) 
+    {
+        this.text = text;
+    }
+
+    public String getLinkToHeaderPicture() 
+    {
+        return linkToHeaderPicture;
+    }
+
+    public void setLinkToHeaderPicture(String linkToHeaderPicture) 
+    {
+        this.linkToHeaderPicture = linkToHeaderPicture;
+    }
+
+    public String getTitle() 
+    {
+        return title;
+    }
+
+    public void setTitle(String title) 
+    {
+        this.title = title;
+    }
+
+    public String getSource() 
+    {
+        return source;
+    }
+
+    public void setSource(String source) 
+    {
+        this.source = source;
+    }
+
+    public String getTimeSincePosted() 
+    {
+        return timeSincePosted;
+    }
+
+    public void setTimeSincePosted(String timeSincePosted) 
+    {
+        this.timeSincePosted = timeSincePosted;
+    }
+
+    public String getLinkToArticle() 
+    {
+        return linkToArticle;
+    }
+
+    public void setLinkToArticle(String linkToArticle) 
+    {
+        this.linkToArticle = linkToArticle;
+    }
+}
