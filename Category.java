ef8939e84208efd6f80f1bc819b7cc61a5bb6399
not-ham123 <98446206+not-ham123@users.noreply.github.com>
2022-06-03 19:46:02 +0700

    Application-Implementation Implemented web scraping for a news source

---
+++ Category.java
@@ -0,0 +1,15 @@
+package com.fun.scrapescrapefx;
+
+public enum Category 
+{
+    NEWEST,
+    COVID,
+    POLITICS,
+    BUSINESS,
+    TECHNOLOGY,
+    HEALTH,
+    SPORTS,
+    ENTERTAINMENT,
+    WORLD,
+    OTHERS
+}
