735eb060b6652c3a996224ec80f5b2e6fc9073ad
not-ham123 <98446206+not-ham123@users.noreply.github.com>
2022-06-04 17:19:52 +0700

    Update ArticleAggregator.java

--- ArticleAggregator.java
+++ ArticleAggregator.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-public interface ArticleAggregator {
+public interface ArticleAggregator 
+{
 
     public Map<Category, List<Article>> getCategorizedArticles();
 
